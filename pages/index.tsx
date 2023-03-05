2aeffc6a27de8381adc76fd1e8c4ad99d5467945
yamasaki junpei <opss0622@gmail.com>
2023-03-05 10:20:07 +0900

    feat: トップページに記事一覧を表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Meta from 'components/meta'
-import cube from "/assets/media/images/cube.png";
+import { Posts } from 'components/posts'
+import { eyeCatchLocal } from 'components/constants'
+import Pagination from 'components/pagination'
+import { getAllPosts } from 'lib/api'
+import { getPlaiceholder } from 'plaiceholder'
+import cube from '/assets/media/images/cube.png'
 
-export default function Home() {
+type Props = {
+  posts: PostsProps[]
+}
+
+type PostsProps = {
+  title: string
+  slug: string
+  id: string
+  eyecatch: {
+    url: string
+    width: number
+    height: number
+    blurDataURL: string
+  }
+}
+
+export default function Home({ posts }: Props) {
   const metaProps = {
     pageImg: cube.src,
     pageImgW: cube.width,
     pageImgH: cube.height,
   }
-  
+
   const heroProps = {
     title: 'CUBE',
     subtitle: 'アウトプットしていくサイト',
@@ -21,7 +42,38 @@ export default function Home() {
       <Container>
         <Meta {...metaProps}></Meta>
         <Hero {...heroProps} />
+        <Posts posts={posts}></Posts>
+        <Pagination prevUrl="" prevText="" nextUrl="/blog/" nextText="More Posts"></Pagination>
       </Container>
     </>
   )
+}
+
+type Posts = {
+  title: string
+  slug: string
+  id: string
+  eyecatch: {
+    url: string
+    width: number
+    height: number
+    blurDataURL?: string
+  }
+}
+
+type AllPosts = Posts[]
+
+export async function getStaticProps() {
+  const limit: number = 4
+  const posts: AllPosts = await getAllPosts(limit)
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) post.eyecatch = eyeCatchLocal
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
