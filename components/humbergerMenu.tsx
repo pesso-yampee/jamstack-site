81c9ba480bdc5a23e17d5a99d870a68547c5c6fc
yamasaki junpei <opss0622@gmail.com>
2023-03-08 08:47:13 +0900

    create: ハンバーガーメニュー作成中

--- components/humbergerMenu.tsx
+++ components/humbergerMenu.tsx
@@ -1,27 +1,14 @@
-import Link from 'next/link'
 import { useState } from 'react'
-import styles from 'styles/nav.module.css'
 
-export default function Nav(): JSX.Element {
+import styles from 'styles/humbergerMenu.module.css'
 
+export function HumbergerMenu(): JSX.Element {
   const [open, setOpen] = useState(false)
-
   return (
-    <nav id="navi">
-      <ul className={styles.list} aria-hidden={!open}>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/blog">Blog</Link>
-        </li>
-      </ul>
+    <>
       <button
         type="button"
-        className={styles.burgerButton}
+        className={styles.flexContainer}
         aria-label={open ? 'メニューを閉じる' : 'メニューを開く'}
         aria-haspopup="true"
         aria-controls="navi"
@@ -32,6 +19,6 @@ export default function Nav(): JSX.Element {
         <span className={styles.line}></span>
         <span className={styles.line}></span>
       </button>
-    </nav>
+    </>
   )
 }
