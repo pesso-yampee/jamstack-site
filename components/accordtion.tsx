72c3f0ae70bd18ba6a2b67f2079d984d9182f45f
yamasaki junpei <opss0622@gmail.com>
2023-03-11 14:31:04 +0900

    fix: アコーディオンの挙動を修正

--- components/accordtion.tsx
+++ components/accordtion.tsx
@@ -1,49 +1,56 @@
 import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState } from 'react'
+import { CSSProperties, ReactNode, useRef, useState } from 'react'
 import styles from 'styles/accordion.module.css'
 
-export default function Accordion() {
+type Props = {
+  heading: string
+  children: ReactNode
+}
+
+export default function Accordion({ heading, children }: Props) {
   const [open, setOpen] = useState(false)
+  const refAnswer = useRef<HTMLDivElement>(null)
+  const refAnswerStyle = {
+    '--answer-text': `${
+      refAnswer.current ? refAnswer.current.scrollHeight : 0
+    }px`,
+  } as CSSProperties
 
   function toggleAccordion() {
     setOpen(!open)
   }
 
   return (
-    <>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <button
-            className={styles.button}
-            aria-expanded={open}
-            aria-controls="accordionPanel-1"
-            id="accordionTab-1"
-            onClick={toggleAccordion}
-          >
-            <span className={styles.title}>プログラミングのポイントについて</span>
-            <FontAwesomeIcon
-              className={styles.icon}
-              icon={faCircleChevronDown}
-              color="var(--gray-25)"
-              width={20}
-              height={20}
-            />
-          </button>
-          <div
-            className={styles.answer}
-            id="accordionPanel-1"
-            aria-hidden={!open}
-            aria-labelledby="accordionPanel-1"
-          >
-            <div className={styles.answer_inner}>
-              <p className={styles.answer_text}>
-                プログラミングのポイントは、作りたいものを作ることです。楽しいことから思いつき、目標とゴールを決め、そこに向かってさまざまな課題を設定していきながら、プログラムを作っていきます。
-              </p>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </>
+    <div className={styles.item}>
+      <h3 className={styles.heading}>
+        <button
+          className={styles.button}
+          aria-expanded={open}
+          aria-controls="accordionPanel-1"
+          id="accordionTab-1"
+          onClick={toggleAccordion}
+        >
+          <span className={styles.title}>{heading}</span>
+          <FontAwesomeIcon
+            className={styles.icon}
+            icon={faCircleChevronDown}
+            color="var(--gray-25)"
+            width={20}
+            height={20}
+          />
+        </button>
+      </h3>
+      <div
+        className={styles.answer}
+        id="accordionPanel-1"
+        aria-hidden={!open}
+        aria-labelledby="accordionPanel-1"
+        ref={refAnswer}
+        style={refAnswerStyle}
+      >
+        <div className={styles.answer_inner}>{children}</div>
+      </div>
+    </div>
   )
 }
